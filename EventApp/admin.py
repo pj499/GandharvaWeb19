5645b128fb6587765052e3687ff5952a57e72d82
Kushal <kushshah777888@gmail.com>
2018-11-21 15:03:59 +0530

    Created SuperUser (Admin)

--- EventApp/admin.py
+++ EventApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import EventMaster,EventDepartment,Department,SponsorMaster,Carousel
 
-# Register your models here.
+admin.site.register(EventMaster)
+admin.site.register(EventDepartment)
+admin.site.register(Department)
+admin.site.register(SponsorMaster)
+admin.site.register(Carousel)
