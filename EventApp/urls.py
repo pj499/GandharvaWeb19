cb7a6aa46ad8fa57460f68a82cef46c0fa543748
Unknown <jakash1997@gmail.com>
2018-11-22 10:23:40 +0530

    App works offline now

--- EventApp/urls.py
+++ EventApp/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url, include
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^events/$',views.event, name='events'),
     url(r'^events/details/$', views.details, name='details'),
+    url(r'^sw.js', (TemplateView.as_view(
+        template_name="sw.js",
+        content_type='application/javascript',
+    )), name='sw.js'),
 ]
