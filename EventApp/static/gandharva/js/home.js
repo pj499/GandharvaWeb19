7574d0512801894ae2418d783dfe0a2cfd8d628a
Sanul-Raskar <sanulraskar@gmail.com>
2018-12-18 20:04:23 +0530

    Added Sponsor section and Event Section

--- EventApp/static/gandharva/js/home.js
+++ EventApp/static/gandharva/js/home.js
@@ -31,14 +31,14 @@ $(document).ready(function () {
 });
 
 
-$(document).ready(function() {
-        // Transition effect for navbar
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 500) {
-              $('.navbar').addClass('solid');
-          } else {
-              $('.navbar').removeClass('solid');
-          }
-        });
+$(document).ready(function () {
+    // Transition effect for navbar
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 500) {
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
+        }
+    });
 });
--- EventApp/static/gandharva/js/main.js
+++ EventApp/static/gandharva/js/main.js
@@ -69,82 +69,82 @@ jQuery(function($) {'use strict',
 
 
 // Google Map Customization
-(function(){
-
-	var map;
-
-	map = new GMaps({
-		el: '#gmap',
-		lat: 43.04446,
-		lng: -76.130791,
-		scrollwheel:false,
-		zoom: 16,
-		zoomControl : false,
-		panControl : false,
-		streetViewControl : false,
-		mapTypeControl: false,
-		overviewMapControl: false,
-		clickable: false
-	});
-
-	var image = 'images/map-icon.png';
-	map.addMarker({
-		lat: 43.04446,
-		lng: -76.130791,
-		icon: image,
-		animation: google.maps.Animation.DROP,
-		verticalAlign: 'bottom',
-		horizontalAlign: 'center',
-		backgroundColor: '#3e8bff',
-	});
-
-
-	var styles = [ 
-
-	{
-		"featureType": "road",
-		"stylers": [
-		{ "color": "#b4b4b4" }
-		]
-	},{
-		"featureType": "water",
-		"stylers": [
-		{ "color": "#d8d8d8" }
-		]
-	},{
-		"featureType": "landscape",
-		"stylers": [
-		{ "color": "#f1f1f1" }
-		]
-	},{
-		"elementType": "labels.text.fill",
-		"stylers": [
-		{ "color": "#000000" }
-		]
-	},{
-		"featureType": "poi",
-		"stylers": [
-		{ "color": "#d9d9d9" }
-		]
-	},{
-		"elementType": "labels.text",
-		"stylers": [
-		{ "saturation": 1 },
-		{ "weight": 0.1 },
-		{ "color": "#000000" }
-		]
-	}
-
-	];
-
-	map.addStyle({
-		styledMapName:"Styled Map",
-		styles: styles,
-		mapTypeId: "map_style"  
-	});
-
-	map.setStyle("map_style");
-}());
+// (function(){
+//
+// 	var map;
+//
+// 	map = new GMaps({
+// 		el: '#gmap',
+// 		lat: 43.04446,
+// 		lng: -76.130791,
+// 		scrollwheel:false,
+// 		zoom: 16,
+// 		zoomControl : false,
+// 		panControl : false,
+// 		streetViewControl : false,
+// 		mapTypeControl: false,
+// 		overviewMapControl: false,
+// 		clickable: false
+// 	});
+//
+// 	var image = 'images/map-icon.png';
+// 	map.addMarker({
+// 		lat: 43.04446,
+// 		lng: -76.130791,
+// 		icon: image,
+// 		animation: google.maps.Animation.DROP,
+// 		verticalAlign: 'bottom',
+// 		horizontalAlign: 'center',
+// 		backgroundColor: '#3e8bff',
+// 	});
+//
+//
+// 	var styles = [
+//
+// 	{
+// 		"featureType": "road",
+// 		"stylers": [
+// 		{ "color": "#b4b4b4" }
+// 		]
+// 	},{
+// 		"featureType": "water",
+// 		"stylers": [
+// 		{ "color": "#d8d8d8" }
+// 		]
+// 	},{
+// 		"featureType": "landscape",
+// 		"stylers": [
+// 		{ "color": "#f1f1f1" }
+// 		]
+// 	},{
+// 		"elementType": "labels.text.fill",
+// 		"stylers": [
+// 		{ "color": "#000000" }
+// 		]
+// 	},{
+// 		"featureType": "poi",
+// 		"stylers": [
+// 		{ "color": "#d9d9d9" }
+// 		]
+// 	},{
+// 		"elementType": "labels.text",
+// 		"stylers": [
+// 		{ "saturation": 1 },
+// 		{ "weight": 0.1 },
+// 		{ "color": "#000000" }
+// 		]
+// 	}
+//
+// 	];
+//
+// 	map.addStyle({
+// 		styledMapName:"Styled Map",
+// 		styles: styles,
+// 		mapTypeId: "map_style"
+// 	});
+//
+// 	map.setStyle("map_style");
+// }());
 
 
 
